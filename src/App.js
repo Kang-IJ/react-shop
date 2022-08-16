000e2bdeaf702635f3d5106c0d0210bc12d23efa
Kang-IJ <rkddl6803@naver.com>
2022-08-16 23:59:04 +0900

    fix: made some changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,128 @@
-import logo from './logo.svg';
+import { createContext, useState } from "react";
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
 import './App.css';
+import data from './data.js';
+import Card from './Card.js';
+import Detail from './routes/Detail.js';
+
+export const Context1 = createContext();
 
 function App() {
+
+  let [shoes, setShoes] = useState(data);
+  let [stock] = useState([10, 11, 12]);
+  let [click, setClick] = useState(0);
+  let [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
+
+  // let arr = [1, 2, 3];
+  // arr.push(4, 5, 6);
+  // console.log(arr);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link onClick={()=> {navigate('/')}}>Home</Nav.Link>
+            <Nav.Link onClick={()=> {navigate('/detail')}}>Detail</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+
+      <Routes>
+        <Route path="/" element={
+          <>
+            <div className="main-bg"></div>
+            <div className="container">
+              <div className="row">
+                  {
+                    shoes.map((a, i)=>{
+                      return (
+                        <Card shoes={shoes[i]} i={i}></Card>
+                      )
+                    })
+                  }
+              </div>
+            </div>
+            {
+              loading === true
+              ? <div>로딩중입니다.</div>
+              : null
+            }
+            <button onClick={()=>{
+              function fetchData(i) {
+                fetch(`https://codingapple1.github.io/shop/data${i}.json`)
+                .then((response) => response.json())
+                .then((data)=> {
+                    let copy = [...shoes];
+                    copy.push(...data);
+                    console.log(copy);
+                    setShoes(copy);
+                  });
+              }
+              setClick(click+1);
+
+              if(click === 0) {
+                fetchData(2);
+              } else if (click === 1){
+                fetchData(3);
+              } else if (click > 1) {
+               alert("더 불러울 상품이 없습니다.");
+              };
+            }}>버튼</button>
+          </>
+        } />
+        <Route path="/detail/:id" element={ 
+          <Context1.Provider value={{stock}}>
+            <Detail shoes={shoes}/>  {/* 이제 context로 감싼 컴포넌트들은 모두 stock state의 사용이 가능하다. */}
+          </Context1.Provider>
+        } />
+        <Route path="*" element={<div>없는 페이지입니다.</div>} />
+        <Route path="/about" element={
+          <About />
+        }>
+          <Route path="member" element={
+            <div>멤버 정보</div>
+          } />
+          <Route path="location" element={
+            <div>장소 정보</div>
+          }/>
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route path="one" element={
+            <div>첫 주문시 양배추즙 서비스</div>
+          } />
+          <Route path="two" element={
+            <div>생일기념 쿠폰받기</div>
+          } />
+        </Route>
+      </Routes>
+
     </div>
   );
 }
+
+function About() {
+  return (
+    <div>
+      <h4>회사 정보</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+function Event() {
+  return (
+    <div>
+      <h4>오늘의 이벤트</h4>
+      <Outlet></Outlet>
+    </div>
+  );
+}
+
 
 export default App;
---
+++ src/routes/Detail.js
@@ -0,0 +1,89 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+
+import { Context1 } from "./../App";
+
+function Detail(props) {
+
+  const states = useContext(Context1); {/* import 해온 state 들이 객체 형태로 남아있을 것 */}
+
+  const {id} = useParams();
+  const value = props.shoes[id].id;
+  const [alert, setAlert] = useState(true);
+  const [input, setInput] = useState('');
+  const [tab, setTab] = useState(0);
+
+  useEffect(()=>{
+    let timer = setTimeout(()=>{  
+      setAlert(false);
+    }, 2000);
+
+      if (isNaN(input) === true) { 
+        alert("숫자를 넣어주세요");
+      }
+
+    
+    
+    return ()=>{
+      clearTimeout(timer);
+    };
+  });
+  
+  return (
+    id < 3 &&  value == id
+    ?  <div className="container">
+      {
+        alert === true 
+        ? <div className="alert alert-warning">
+            2초 이내 구매 시 할인
+          </div>
+        : null
+      }
+        
+        <div className="row">
+          <div className="col-md-6">
+            <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.shoes[id].id + 1) + '.jpg' } width="100%" alt="img"/>
+            <input onChange={(e)=>{setInput(e.target.value);}} />
+          </div>
+          <div className="col-md-6 mt-4">
+            <h4 className="pt-5">{props.shoes[id].title}</h4>
+            <p>{props.shoes[id].content}</p>
+            <p>{props.shoes[id].price}원</p>
+            <button className="btn btn-danger">주문하기</button>
+          </div>
+        </div>
+        <Nav variant="tabs"  defaultActiveKey="link0">
+          <Nav.Item>
+            <Nav.Link onClick={()=>{setTab(0)}} eventKey="link0">버튼0</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{setTab(1)}} eventKey="link1">버튼1</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link onClick={()=>{setTab(2)}} eventKey="link2">버튼2</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <TabContent tab={ tab }/>
+
+      </div>
+    : <div>없는 페이지입니다.</div>
+  );
+}
+function TabContent({tab}) {
+  let [fade, setFade] = useState('');
+  
+  useEffect(()=>{
+    setTimeout(()=>{setFade("end")}, 10);
+    return () => {
+      setFade("");
+    }
+  }, [tab])
+  return ( <div className={"start " + fade }>
+  {[<div>내용0</div>, <div>내용1</div>, <div>내용2</div>][tab]}
+  </div>
+  )
+}
+
+
+export default Detail;
